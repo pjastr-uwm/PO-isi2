d0922575c014e429e6ad3c053d9f69d7c7925e5d
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2023-10-25 11:45:52 +0200

    11

---
+++ lab4/src/TestCar3.java
@@ -0,0 +1,11 @@
+public class TestCar3 {
+    public static void main(String[] args) {
+        Car3[] tablica = {null, null, null};
+        System.out.println(tablica[0].brand);
+    }
+}
+
+class Car3
+{
+    public String brand;
+}
---
+++ lab4/src/TestCat.java
@@ -0,0 +1,29 @@
+public class TestCat {
+
+    public static void main(String[] args) {
+        Cat c1 = new Cat();
+        Cat c2 = c1.createCat("Azor");
+        System.out.println(c2.name);
+        Cat c3 = c1.createCat(null);
+        System.out.println(c3);
+    }
+}
+
+class Cat
+{
+    String name;
+
+    public Cat createCat(String value)
+    {
+        Cat temp = new Cat();
+        if (value != null)
+        {
+            temp.name = value;
+        }
+        else
+        {
+            return null;
+        }
+        return temp;
+    }
+}
