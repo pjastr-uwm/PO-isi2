79694ce5c8d25f1631f3998c16d15618a31a7443
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2023-11-08 10:29:23 +0100

    zad1

---
+++ lab5/src/pl/books/zad1/Book.java
@@ -0,0 +1,17 @@
+package pl.books.zad1;
+
+class Book {
+    private String title;
+    private String author;
+    private int publicationYear;
+
+    public Book(){
+
+    }
+
+    public Book(String title, String author, int publicationYear){
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+    }
+}
---
+++ lab5/src/pl/books/zad1/TestBook.java
@@ -0,0 +1,9 @@
+package pl.books.zad1;
+
+class TestBook {
+
+    public static void main(String[] args) {
+        Book b1 = new Book();
+        Book b2 = new Book("Pan Tadeusz", "Adam Mickiewicz", 1852);
+    }
+}
