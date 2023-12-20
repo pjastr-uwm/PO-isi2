b13ce6aeb7ffde8be6a4ea3728b7f8eff3d360c7
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2023-12-20 10:26:15 +0100

    zad1

---
+++ lab11/src/komparator/Osoba.java
@@ -0,0 +1,27 @@
+package komparator;
+
+public class Osoba {
+
+    private String imie;
+    private int wiek;
+    private double wzrost;
+
+    public Osoba(String imie, int wiek, double wzrost) {
+        this.imie = imie;
+        this.wiek = wiek;
+        this.wzrost = wzrost;
+    }
+
+    @Override
+    public String toString() {
+        return "Osoba{" +
+                "imie='" + imie + '\'' +
+                ", wiek=" + wiek +
+                ", wzrost=" + wzrost +
+                '}';
+    }
+
+    public int getWiek() {
+        return wiek;
+    }
+}
---
+++ lab11/src/komparator/TestOsoba.java
@@ -0,0 +1,24 @@
+package komparator;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class TestOsoba {
+
+    public static void main(String[] args) {
+        Osoba[] tab = new Osoba[5];
+        tab[0] = new Osoba("AA", 45, 156);
+        tab[1] = new Osoba("BB", 21, 167);
+        tab[2] = new Osoba("AA", 33, 172);
+        tab[3] = new Osoba("CC", 44, 156);
+        tab[4] = new Osoba("DD", 21, 156);
+        for(var elem: tab){
+            System.out.println(elem);
+        }
+        Arrays.sort(tab, new WiekComparator());
+        System.out.println("Aftre sorting");
+        for(var elem: tab){
+            System.out.println(elem);
+        }
+    }
+}
