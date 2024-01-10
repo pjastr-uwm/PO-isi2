c145d6fa621ac7ca5ec61fec4d8e75ecb0ea9557
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2024-01-10 11:09:27 +0100

    k1

---
+++ lab13/src/kolekcje/zadAK1/Alg.java
@@ -0,0 +1,21 @@
+package kolekcje.zadAK1;
+
+import java.util.HashMap;
+
+public class Alg {
+
+    public <K,V> HashMap<V, Integer> countValueOccurrences(HashMap<K,V> arg){
+        HashMap<V, Integer> temp = new HashMap<>();
+        for(var values: arg.values()){
+            int counter =0;
+            for(var values2: arg.values()){
+                if (values.equals(values2)){
+                    counter++;
+                }
+            }
+            temp.put(values, counter);
+        }
+
+        return temp;
+    }
+}
---
+++ lab13/src/kolekcje/zadAK1/TestK1.java
@@ -0,0 +1,17 @@
+package kolekcje.zadAK1;
+
+import java.util.HashMap;
+
+public class TestK1 {
+
+    public static void main(String[] args) {
+        Alg a1 = new Alg();
+        HashMap<Integer, String> h1 = new HashMap<>();
+        h1.put(1, "abc");
+        h1.put(20, "XY");
+        h1.put(3, "abc");
+        System.out.println(h1);
+        HashMap<String, Integer> result = a1.countValueOccurrences(h1);
+        System.out.println(result);
+    }
+}
